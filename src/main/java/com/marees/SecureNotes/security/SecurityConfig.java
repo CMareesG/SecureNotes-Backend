d968f1bc3d68b88a4e7a9f0892beeee1520ae6f2
mareeswaraganapathy <mareeswaraganapathy@gmail.com>
2025-08-29 22:52:23 +0530

    Update SecurityConfig.java

--- src/main/java/com/marees/SecureNotes/security/SecurityConfig.java
+++ src/main/java/com/marees/SecureNotes/security/SecurityConfig.java
@@ -60,15 +60,15 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        // http.csrf(csrf ->
-        //         csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-        //                 .ignoringRequestMatchers("/api/auth/public/**")
-        // );
-        http.csrf(AbstractHttpConfigurer::disable);
+        http.csrf(csrf ->
+                csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                        .ignoringRequestMatchers("/api/auth/public/**")
+        );
+        //http.csrf(AbstractHttpConfigurer::disable);
         http.authorizeHttpRequests((requests)
                 -> requests
                 .requestMatchers("/api/admin/**").hasRole("ADMIN")
-                //.requestMatchers("/api/csrf-token").permitAll()
+                .requestMatchers("/api/csrf-token").permitAll()
                 .requestMatchers("/api/auth/public/**").permitAll()
                 .requestMatchers("/api/auth/public/signin").permitAll()
                 .requestMatchers("/api/auth/public/forget-password").permitAll()
