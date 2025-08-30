227f0d358ebacec77c413635beec9f5802236658
mareeswaraganapathy <mareeswaraganapathy@gmail.com>
2025-08-30 11:05:57 +0530

    Update CsrfController.java

--- src/main/java/com/marees/SecureNotes/controller/CsrfController.java
+++ src/main/java/com/marees/SecureNotes/controller/CsrfController.java
@@ -1,16 +1,16 @@
-package com.marees.SecureNotes.controller;
+// package com.marees.SecureNotes.controller;
 
-import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.security.web.csrf.CsrfToken;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+// import jakarta.servlet.http.HttpServletRequest;
+// import org.springframework.security.web.csrf.CsrfToken;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class CsrfController {
+// @RestController
+// public class CsrfController {
 
-    @GetMapping("/api/csrf-token")
-    public CsrfToken csrfToken(HttpServletRequest request)
-    {
-        return (CsrfToken) request.getAttribute(CsrfToken.class.getName());
-    }
-}
+//     @GetMapping("/api/csrf-token")
+//     public CsrfToken csrfToken(HttpServletRequest request)
+//     {
+//         return (CsrfToken) request.getAttribute(CsrfToken.class.getName());
+//     }
+// }
